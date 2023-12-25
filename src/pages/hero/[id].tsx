bebcc55250aa5ee058cee08a0cadbd86595df19d
ProCod <procod0101@gmail.com>
2023-12-25 13:26:51 +0200

    init

---
+++ src/pages/hero/[id].tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+
+
+const HeroPage:NextPage=()=>{
+    const {replace,asPath}=useRouter()
+    console.log(asPath)
+    return (
+    <div>
+        <h1>HeroPage</h1>
+        <button onClick={()=>replace('/')}>GO BACK</button>
+
+    </div>
+    )
+    
+}
+
+export default HeroPage
