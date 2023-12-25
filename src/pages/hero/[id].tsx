77419a07341a26080ff319f13613bff2bd880cee
ProCod <procod0101@gmail.com>
2023-12-25 13:46:41 +0200

    first commit

--- src/pages/hero/[id].tsx
+++ src/pages/hero/[id].tsx
@@ -3,16 +3,14 @@ import { useRouter } from "next/router";
 
 
 const HeroPage:NextPage=()=>{
-    const {replace,asPath}=useRouter()
-    console.log(asPath)
+    const {replace,asPath}=useRouter()   
     return (
     <div>
         <h1>HeroPage</h1>
         <button onClick={()=>replace('/')}>GO BACK</button>
 
     </div>
-    )
-    
+    )    
 }
 
 export default HeroPage
